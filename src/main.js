a8465c4537071d43b9fdc3aceeef3f3c5a79f60a
Rohan Adla <radla@umass.edu>
2022-03-25 15:50:50 -0400

    active icon functional

---
+++ src/main.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    init();
+    icons.forEach(element => {
+        element.addEventListener('click', activateIcon)
+    });
+}
+
+let icons = [];
+
+function init() {
+    icons = document.querySelectorAll(".icon");
+}
+
+function activateIcon() {
+    icons.forEach(i => {i.classList.remove('icon-active')})
+    icons.forEach(i => {i.classList.add('icon')})
+    this.classList.add('icon-active')
+    this.classList.remove('icon')
+}
